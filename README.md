ae1106035313916fc872d83d69959f2f10c886be
vsan <vadim.sanzharov@graphics.cs.msu.ru>
2021-11-17 19:01:56 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Ray tracing sample using Vulkan API
+This project provides ray tracing applications sample 
+
+
+## Build
+
+Build as you would any other CMake-based project. For example on Linux:
+```
+cd vk_graphics_rt
+mkdir build
+cd build
+cmake ..
+make -j8
+```
+
+On Windows you can directly open CMakeLists.txt as a project in recent versions of Visual Studio. Mingw-w64 building is also supported.
+
+Executable will be built in *bin* subdirectory - *vk_graphics_rt/bin/renderer*
+
+## Dependencies
+### Vulkan 
+SDK can be downloaded from https://vulkan.lunarg.com/
+
+### GLFW 
+https://www.glfw.org/
+
+Linux - you can install it as a package on most systems, for example: 
+```
+sudo apt-get install libglfw3-dev
+```
+
+Windows - you don't need to do anything, binaries and headers are included in this repo (see external/glfw directory).
+
+### Additional dependencies 
+Can be installed by running provided clone_dependencies.bat script. So, if you clone this repo into vk_graphics_rt directory:
+
+```
+cd vk_graphics_basic
+clone_dependencies.bat
+```
+
+Or you can get them manually:
+
+#### volk:
+```
+cd vk_graphics_rt/external
+git clone --depth 1 https://github.com/zeux/volk.git
+```
+
+#### imgui:
+```
+cd vk_graphics_rt/external
+git clone --depth 1 https://github.com/ocornut/imgui.git
+```
+
+#### vkutils:
+```
+cd vk_graphics_rt/external
+git clone --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
+```
+
+### Single header libraries
+This project also uses LiteMath and stb_image which are included in this repo.
+
+https://github.com/msu-graphics-group/LiteMath
+
+https://github.com/nothings/stb
+
---
+++ src/render/scene_mgr.h
@@ -0,0 +1,101 @@
+#ifndef CHIMERA_SCENE_MGR_H
+#define CHIMERA_SCENE_MGR_H
+
+#include <vector>
+
+#include <geom/vk_mesh.h>
+#include "LiteMath.h"
+#include <vk_copy.h>
+
+#include "../loader_utils/hydraxml.h"
+#include "../resources/shaders/common.h"
+
+struct InstanceInfo
+{
+  uint32_t inst_id = 0u;
+  uint32_t mesh_id = 0u;
+  VkDeviceSize instBufOffset = 0u;
+  bool renderMark = false;
+};
+
+struct SceneManager
+{
+  SceneManager(VkDevice a_device, VkPhysicalDevice a_physDevice, uint32_t a_transferQId, uint32_t a_graphicsQId,
+    bool debug = false);
+  ~SceneManager() { DestroyScene(); }
+
+  bool LoadSceneXML(const std::string &scenePath, bool transpose = true);
+  void LoadSingleTriangle();
+
+  uint32_t AddMeshFromFile(const std::string& meshPath);
+  uint32_t AddMeshFromData(cmesh::SimpleMesh &meshData);
+
+  uint32_t InstanceMesh(uint32_t meshId, const LiteMath::float4x4 &matrix, bool markForRender = true);
+
+  void MarkInstance(uint32_t instId);
+  void UnmarkInstance(uint32_t instId);
+
+  void DrawMarkedInstances();
+
+  void DestroyScene();
+
+  VkPipelineVertexInputStateCreateInfo GetPipelineVertexInputStateCreateInfo() { return m_pMeshData->VertexInputLayout();}
+
+  VkBuffer GetVertexBuffer() const { return m_geoVertBuf; }
+  VkBuffer GetIndexBuffer()  const { return m_geoIdxBuf; }
+  VkBuffer GetMeshInfoBuffer()  const { return m_meshInfoBuf; }
+  auto GetMeshData() const { return m_pMeshData; };
+  auto GetCopyHelper() { return  m_pCopyHelper; }
+
+  uint32_t MeshesNum() const {return (uint32_t)m_meshInfos.size();}
+  uint32_t InstancesNum() const {return (uint32_t)m_instanceInfos.size();}
+
+  hydra_xml::Camera GetCamera(uint32_t camId) const;
+  MeshInfo GetMeshInfo(uint32_t meshId) const {assert(meshId < m_meshInfos.size()); return m_meshInfos[meshId];}
+  LiteMath::Box4f GetMeshBbox(uint32_t meshId) const {assert(meshId < m_meshBboxes.size()); return m_meshBboxes[meshId];}
+  InstanceInfo GetInstanceInfo(uint32_t instId) const {assert(instId < m_instanceInfos.size()); return m_instanceInfos[instId];}
+  LiteMath::Box4f GetInstanceBbox(uint32_t instId) const {assert(instId < m_instanceBboxes.size()); return m_instanceBboxes[instId];}
+  LiteMath::float4x4 GetInstanceMatrix(uint32_t instId) const {assert(instId < m_instanceMatrices.size()); return m_instanceMatrices[instId];}
+  LiteMath::Box4f GetSceneBbox() const {return sceneBbox;}
+
+private:
+  void LoadGeoDataOnGPU();
+
+  std::vector<MeshInfo> m_meshInfos = {};
+  std::vector<LiteMath::Box4f> m_meshBboxes = {};
+  std::shared_ptr<IMeshData> m_pMeshData = nullptr;
+
+  std::vector<InstanceInfo> m_instanceInfos = {};
+  std::vector<LiteMath::Box4f> m_instanceBboxes = {};
+  std::vector<LiteMath::float4x4> m_instanceMatrices = {};
+
+  std::vector<hydra_xml::Camera> m_sceneCameras = {};
+  LiteMath::Box4f sceneBbox;
+
+  uint32_t m_totalVertices = 0u;
+  uint32_t m_totalIndices  = 0u;
+
+  VkBuffer m_geoVertBuf = VK_NULL_HANDLE;
+  VkBuffer m_geoIdxBuf  = VK_NULL_HANDLE;
+  VkBuffer m_meshInfoBuf  = VK_NULL_HANDLE;
+  VkBuffer m_instanceMatricesBuffer = VK_NULL_HANDLE;
+  VkDeviceMemory m_geoMemAlloc = VK_NULL_HANDLE;
+
+  VkDevice m_device = VK_NULL_HANDLE;
+  VkPhysicalDevice m_physDevice = VK_NULL_HANDLE;
+  uint32_t m_transferQId = UINT32_MAX;
+  VkQueue m_transferQ = VK_NULL_HANDLE;
+
+  uint32_t m_graphicsQId = UINT32_MAX;
+  VkQueue m_graphicsQ = VK_NULL_HANDLE;
+  std::shared_ptr<vk_utils::ICopyEngine> m_pCopyHelper;
+
+  bool m_debug = false;
+  // for debugging
+  struct Vertex
+  {
+    float pos[3];
+  };
+};
+
+#endif//CHIMERA_SCENE_MGR_H
