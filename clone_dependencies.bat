d1ed50d524a67623cc71931ad24ebe41051e790a
Vladimir Frolov <vova@frolov.pp.ru>
2024-10-24 19:50:00 +0300

    fix CMake

--- clone_dependencies.bat
+++
@@ -1,4 +0,0 @@
-cd external
-git clone --depth 1 https://github.com/zeux/volk.git
-git clone --depth 1 https://github.com/ocornut/imgui.git
-git clone --branch slicer --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
